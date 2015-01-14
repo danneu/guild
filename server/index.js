f288b83a79195e512d52adaa8d91277f1c3cd75d
Dan Neumann <danrodneu@gmail.com>
2015-01-13 18:09:39 -0600

    Use koa's default err handler for now

--- server/index.js
+++ server/index.js
@@ -47,10 +47,10 @@ var log = require('./logger');
 var cache = require('./cache')(log);
 
 // Catch and log all errors that bubble up to koa
-app.on('error', function(err) {
-  log.error(err, 'Error');
-  console.error('Error:', err, err.stack);
-});
+// app.on('error', function(err) {
+//   log.error(err, 'Error');
+//   console.error('Error:', err, err.stack);
+// });
 
 app.use(function*(next) {
   var start = Date.now();
