8004138e5947b38ff0d807b215555ae35577ff37
Dan Neumann <danrodneu@gmail.com>
2015-01-20 21:08:16 -0600

    Parse config exports into numbers

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = parseInt(process.env.PORT, 10) || 3000;
 // Format: postgres://<user>:<pass>@<host>:<port>/<dbname>
 exports.DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/guild';
 // 'development' | 'production'
@@ -8,12 +8,12 @@ exports.RECAPTCHA_SITEKEY = process.env.RECAPTCHA_SITEKEY;
 exports.RECAPTCHA_SITESECRET = process.env.RECAPTCHA_SITESECRET;
 
 // Various configurable forum settings
-exports.MIN_TOPIC_TITLE_LENGTH = process.env.MIN_TOPIC_TITLE_LENGTH || 3;
-exports.MAX_TOPIC_TITLE_LENGTH = process.env.MAX_TOPIC_TITLE_LENGTH || 50;
-exports.MIN_POST_LENGTH = process.env.MIN_POST_LENGTH || 3;
-exports.MAX_POST_LENGTH = process.env.MAX_POST_LENGTH || 65535;
-exports.MIN_UNAME_LENGTH = process.env.MIN_UNAME_LENGTH || 2;
-exports.MAX_UNAME_LENGTH = process.env.MAX_UNAME_LENGTH || 15;
+exports.MIN_TOPIC_TITLE_LENGTH = parseInt(process.env.MIN_TOPIC_TITLE_LENGTH, 10) || 3;
+exports.MAX_TOPIC_TITLE_LENGTH = parseInt(process.env.MAX_TOPIC_TITLE_LENGTH, 10) || 150;
+exports.MIN_POST_LENGTH = parseInt(process.env.MIN_POST_LENGTH, 10) || 3;
+exports.MAX_POST_LENGTH = parseInt(process.env.MAX_POST_LENGTH, 10) || 65535;
+exports.MIN_UNAME_LENGTH = parseInt(process.env.MIN_UNAME_LENGTH, 10) || 2;
+exports.MAX_UNAME_LENGTH = parseInt(process.env.MAX_UNAME_LENGTH, 10) || 15;
 
 // Determines the link in password reset token email
 exports.HOST = process.env.HOST || ('http://localhost:' + exports.PORT);
@@ -23,10 +23,10 @@ exports.AWS_SECRET = process.env.AWS_SECRET;
 exports.FROM_EMAIL = process.env.FROM_EMAIL;
 
 // How many posts/PMs to display per page in topics/convos
-exports.POSTS_PER_PAGE = process.env.POSTS_PER_PAGE || 20;
+exports.POSTS_PER_PAGE = parseInt(process.env.POSTS_PER_PAGE, 10) || 20;
 // How many recent posts to display on user profile
-exports.RECENT_POSTS_PER_PAGE = process.env.RECENT_POSTS_PER_PAGE || 5;
-exports.CONVOS_PER_PAGE = process.env.CONVOS_PER_PAGE || 10;
+exports.RECENT_POSTS_PER_PAGE = parseInt(process.env.RECENT_POSTS_PER_PAGE, 10) || 5;
+exports.CONVOS_PER_PAGE = parseInt(process.env.CONVOS_PER_PAGE, 10) || 10;
 
 // newrelic
 exports.NEW_RELIC_LICENSE_KEY = process.env.NEW_RELIC_LICENSE_KEY;
