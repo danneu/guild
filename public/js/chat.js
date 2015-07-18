b669624f2b8432b7567d894d1721ff2c25573441
Dan Neumann <danrodneu@gmail.com>
2015-07-18 15:22:39 -0500

    Lighten uname colors in chatbox

--- public/js/chat.js
+++ public/js/chat.js
@@ -47,6 +47,26 @@ helpers.safeAutolink = function(text) {
 helpers.toMD5HexColor = function(s) {
   return '#' + md5(s).slice(0, 6);
 };
+helpers.lightenColor = function(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;
+    G = (G<255)?G:255;
+    B = (B<255)?B:255;
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
 
 // props:
 // - muteList
@@ -321,7 +341,7 @@ var App = React.createClass({
                           {
                             className: 'message-uname',
                             style: {
-                              color: helpers.toMD5HexColor(m.user.uname)
+                              color: helpers.lightenColor(helpers.toMD5HexColor(m.user.uname), 25)
                             }
                           },
                           m.user.uname + ':'
