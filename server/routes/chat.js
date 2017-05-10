3e8fb3f3e641eac6928ed77de19563ed726b70e2
Dan Neumann <danrodneu@gmail.com>
2017-05-10 06:18:41 -0500

    Replace _.padLeft

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,46 +1,46 @@
-'use strict';
+'use strict'
 // 3rd
-const Router = require('koa-router');
+const Router = require('koa-router')
 // 1st
-const cancan = require('../cancan');
-const db = require('../db');
+const cancan = require('../cancan')
+const db = require('../db')
 
 ////////////////////////////////////////////////////////////
 
-const router = new Router();
+const router = new Router()
 
 ////////////////////////////////////////////////////////////
 
 router.get('/chatlogs', async (ctx) => {
-  ctx.assertAuthorized(ctx.currUser, 'READ_CHATLOGS');
-  const logs = await db.chat.getChatLogDays();
+  ctx.assertAuthorized(ctx.currUser, 'READ_CHATLOGS')
+  const logs = await db.chat.getChatLogDays()
   await ctx.render('list_chatlogs', {
     ctx,
-    logs: logs
-  });
-});
+    logs
+  })
+})
 
 ////////////////////////////////////////////////////////////
 
 // :when is 'YYYY-MM-DD'
 router.get('/chatlogs/:when', async (ctx) => {
   // Temporarily disable
-  ctx.assertAuthorized(ctx.currUser, 'READ_CHATLOGS');
+  ctx.assertAuthorized(ctx.currUser, 'READ_CHATLOGS')
   // TODO: Validate
   ctx.validateParam('when')
-    .match(/\d{4}-\d{2}-\d{2}/, 'Invalid date format');
+    .match(/\d{4}-\d{2}-\d{2}/, 'Invalid date format')
 
-  const log = await db.chat.findLogByDateTrunc(ctx.vals.when);
-  ctx.assert(log, 404);
-  ctx.assert(log.length > 0, 404);
+  const log = await db.chat.findLogByDateTrunc(ctx.vals.when)
+  ctx.assert(log, 404)
+  ctx.assert(log.length > 0, 404)
 
   await ctx.render('show_chatlog', {
     ctx,
-    log: log,
+    log,
     when: log[0].when
-  });
-});
+  })
+})
 
 ////////////////////////////////////////////////////////////
 
-module.exports = router;
+module.exports = router
