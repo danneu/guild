35355c387617f983af710ea14195dbad85469c5a
Dan Neumann <danrodneu@gmail.com>
2017-05-10 07:54:20 -0500

    Modernize gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,49 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var rev = require('gulp-rev');
-var minifyCSS = require('gulp-cssnano');
-var uglifyJS = require('gulp-uglify');
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const rev = require('gulp-rev')
+const minifyCss = require('gulp-cssnano')
+const uglifyJs = require('gulp-uglify')
+const del = require('del')
+const vinylPaths = require('vinyl-paths')
 
-gulp.task('copy-fonts', function() {
-  var fontPaths = [
-    './public/vendor/bootstrap/fonts/**',
-    './public/vendor/font-awesome/fonts/**'
-  ];
-  return gulp.src(fontPaths)
-      .pipe(gulp.dest('./dist/fonts/'));
-});
+gulp.task('reset-dist', () => {
+  return del(['dist/**/*'])
+})
 
-gulp.task('build-css', ['copy-fonts'], function() {
-  var cssPaths = [
-    './public/vendor/bootstrap/css/bootstrap.css',
-    './public/vendor/bootstrap/css/bootstrap-theme.css',
-    './public/vendor/font-awesome/css/font-awesome.css',
-    './public/vendor/bootstrap-markdown/css/bootstrap-markdown.min.css',
-    './public/css/bootstrap_overrides.css',
-    './public/css/general.css'
-  ];
+gulp.task('copy-fonts:bootstrap', ['reset-dist'], () => {
+  return gulp.src(['public/vendor/bootstrap/fonts/**'], {
+    base: 'public/vendor/bootstrap'
+  })
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('copy-fonts:font-awesome', ['reset-dist'], () => {
+  return gulp.src(['public/vendor/font-awesome/fonts/**'], {
+    base: 'public/vendor/font-awesome'
+  })
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('copy-fonts', ['copy-fonts:bootstrap', 'copy-fonts:font-awesome'])
+
+
+gulp.task('build-css', ['copy-fonts'], () => {
+  const cssPaths = [
+    'public/vendor/bootstrap/css/bootstrap.css',
+    'public/vendor/bootstrap/css/bootstrap-theme.css',
+    'public/vendor/font-awesome/css/font-awesome.css',
+    'public/vendor/bootstrap-markdown/css/bootstrap-markdown.min.css',
+    'public/css/bootstrap_overrides.css',
+    'public/css/general.css'
+  ]
   return gulp.src(cssPaths)
       .pipe(concat('all.css'))
-      .pipe(minifyCSS())
-      .pipe(gulp.dest('./dist/'));
-});
+      .pipe(minifyCss())
+      .pipe(gulp.dest('dist'))
+})
 
-gulp.task('build-js', function() {
-  var jsPaths = [
+gulp.task('build-js', () => {
+  const jsPaths = [
     'public/vendor/jquery/jquery-2.1.3.min.js',
     'public/vendor/jquery-hotkeys/jquery.hotkeys.js',
     'public/vendor/timeago/jquery.timeago.js',
@@ -43,32 +57,36 @@ gulp.task('build-js', function() {
     'public/vendor/xbbcode/xbbcode/bbcode.js', // Symlinked to server/bbcode.js
     // Don't bundle typeahead since it's just used on edit_topic.js right now
     // 'public/vendor/typeahead/typeahead.bundle.js',
-    'public/js/bbcode_editor.js',
-  ];
+    'public/js/bbcode_editor.js'
+  ]
   return gulp.src(jsPaths)
-      .pipe(concat('all.js'))
-      .pipe(uglifyJS())
-      .pipe(gulp.dest('./dist/'));
-});
+    .pipe(concat('all.js'))
+    .pipe(uglifyJs())
+    .pipe(gulp.dest('dist'))
+})
 
-gulp.task('build-chat-js', function() {
-  var paths = [
+gulp.task('build-chat-js', () => {
+  const paths = [
     'public/js/chat.js'
-  ];
+  ]
   return gulp.src(paths)
     .pipe(concat('chat.js'))
-    .pipe(uglifyJS())
-    .pipe(gulp.dest('./dist/'));
-});
+    .pipe(uglifyJs())
+    .pipe(gulp.dest('dist'))
+})
 
 //
 // Note: I usually have to run `gulp build-assets` twice for it
 // to work. Need to look into why.
 //
-gulp.task('build-assets', ['build-css', 'build-js', 'build-chat-js'], function() {
+gulp.task('build-assets', [
+  'reset-dist',
+  'build-css', 'build-js', 'build-chat-js'
+], () => {
   return gulp.src(['dist/all.css', 'dist/all.js', 'dist/chat.js'])
+  .pipe(vinylPaths(del))
   .pipe(rev())
   .pipe(gulp.dest('dist'))
   .pipe(rev.manifest())
-  .pipe(gulp.dest('dist'));
-});
+  .pipe(gulp.dest('dist'))
+})
