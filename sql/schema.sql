3e2a9f9b2e840ee1c97db3b71758956626ce11b0
Legend <legend.begins@yahoo.com>
2025-01-03 00:05:58 -0600

    Better alt table creation in schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1032,10 +1032,19 @@ $$ LANGUAGE SQL IMMUTABLE;
 ------------------------------------------------------------
 ------------------------------------------------------------
 
-CREATE TABLE alts ( 
-  id             serial             PRIMARY KEY,
-  owner_id        serial             NOT NULL
+CREATE TABLE alts (
+  id         integer     PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+  owner_id   integer     NOT NULL REFERENCES users(id),
+  created_at timestamptz  NOT NULL DEFAULT NOW()
 );
+
+-- There's already an index on primary keys but lets add one for the foreign key.
+CREATE INDEX idx_alts_owner_id ON alts(owner_id);
+
+-- Initialize with existing users
+INSERT INTO alts (id, owner_id)
+SELECT id, id
+FROM users
 
 -- CREATE OR REPLACE FUNCTION strip_quotes(markup text) RETURNS text
 -- AS $$
