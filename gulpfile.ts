16fdebfa2755e1610ca38831dbcc9f2850cf3c40
Dan Neumann <danrodneu@gmail.com>
2025-06-13 13:03:03 -0500

    make server copy wait on completion

--- gulpfile.ts
+++ gulpfile.ts
@@ -27,11 +27,9 @@ function copyVendorDeps() {
   ]).pipe(dest("public/vendor/"));
 }
 
-function copyServerFiles() {
-  return Promise.all([
-    src("server/bbcode.js").pipe(dest("public/vendor/xbbcode/xbbcode/")),
-    src("server/ago.js").pipe(dest("public/js/")),
-  ]);
+async function copyServerFiles() {
+  await src("server/bbcode.js").pipe(dest("public/vendor/xbbcode/xbbcode/"));
+  await src("server/ago.js").pipe(dest("public/js/"));
 }
 
 function buildCss() {
@@ -59,8 +57,7 @@ function buildJs() {
     "public/vendor/jquery-appear/jquery.appear.js",
     "public/vendor/bootstrap/js/bootstrap.js",
     // 'public/vendor/js/bootstrap.js',
-    // Symlinked to node_modules/autolinker
-    "public/vendor/autolinker/dist/autolinker.min.js",
+    "public/vendor/autolinker.min.js",
     "public/vendor/xbbcode/xbbcode/bbcode.js", // Copied from server/bbcode.js
     // Don't bundle typeahead since it's just used on edit_topic.js right now
     // 'public/vendor/typeahead/typeahead.bundle.js',
